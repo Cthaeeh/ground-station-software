c118ead08086d766b763a8a94ce15520816fe7dd
Kai <u7moench@gmail.com>
2017-06-21 23:38:35 +0200

    GSS updated TODO's . Forgot about that one.

--- src/main/Main.java
+++ src/main/Main.java
@@ -27,11 +27,12 @@ public class Main extends Application {
 //          // TODO visualize bitFlags, enums.
 //          // TODO somewhere make sure 2's complement is mentioned.
 //     6    // TODO add CRC 16 functionality.
+            // TODO Fix the mess in the SerialCOmmThread.
+            // TODO byte stuffing.
 //     7    // TODO add time functionality.
 //          // TODO add a way of exchanging. a config file while program is running. very difficult u have to restart the TM/TC thread and kill all the visualizations. Possible but ...
 //          // TODO NOTE that an uneven number of bytes for a value is discouraged.
             // TODO add junit test everywhere. U can even test the serial stuff with linux and virtual ports.
-            // TODO accept variable length messages, maybe somewhere decode length in msg.
 
             // later maybe
             // TODO add support for float and double and unsigned int maybe ??
