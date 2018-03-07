a5f9e2d5f3ff23f9b31706aedb6bef9b7e0a3811
Kai Hofmann <u7moench@gmail.com>
2018-03-07 17:30:10 +0100

    Add Shortcuts to DataSourceSelection

--- src/visualization/DataSourceSelectionControl.java
+++ src/visualization/DataSourceSelectionControl.java
@@ -7,7 +7,10 @@ import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -18,12 +21,15 @@ import java.util.ResourceBundle;
  * Created by Kai on 27.04.2017.
  * Handles the Selection of one or more DataSources and the corresponding way how to display them.
  */
-public class DataSourceSelectionControl implements Initializable{
+public class DataSourceSelectionControl implements Initializable {
+
+    @FXML
+    private StackPane pane;
 
     //TODO warn users that not all sources can be displayed the same way.
 
     //Member
-    private DataModel model ;
+    private DataModel model;
     private DataVisualization dataVisualization;
 
     //FXML elements
@@ -51,21 +57,34 @@ public class DataSourceSelectionControl implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initializeChoiceBox();
+        initializeShortcuts();
+    }
+
+    private void initializeShortcuts() {
+        pane.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            System.out.println("got key event");
+            if(event.getCode()== KeyCode.ENTER) btnOkayClick();
+            if(event.getCode()==KeyCode.ESCAPE) {
+                Stage stage = (Stage) presentationMode.getScene().getWindow();
+                stage.close();
+            }
+        });
     }
 
     void initModel(DataModel model) {
         if (this.model != null) {
             throw new IllegalStateException("Model can only be initialized once");
         }
-        this.model = model ;
+        this.model = model;
         initializeSelectionTable();
     }
 
     /**
      * If you want to get informed about the dataVisualization the user has chosen, pass a lambda with this method.
+     *
      * @param dataVisualization the one who requested which data should be shown in which way.
      */
-    void register(DataVisualization dataVisualization){
+    void register(DataVisualization dataVisualization) {
         this.dataVisualization = dataVisualization;
     }
 
@@ -88,20 +107,20 @@ public class DataSourceSelectionControl implements Initializable{
         FilteredList<DataSource> filteredSources =
                 new FilteredList<>(model.getDataSources(), p -> true);
 
-        filteredText.textProperty().addListener((observable,oldValue,newValue)-> {
-            dataSourceSelectionTable.getSelectionModel().clearSelection();
-            filteredSources.setPredicate( dataSource -> {
-                //Show everything if filteredText is empty
-                if(newValue == null|| newValue.isEmpty()){
-                    return true;
+        filteredText.textProperty().addListener((observable, oldValue, newValue) -> {
+                    dataSourceSelectionTable.getSelectionModel().clearSelection();
+                    filteredSources.setPredicate(dataSource -> {
+                        //Show everything if filteredText is empty
+                        if (newValue == null || newValue.isEmpty()) {
+                            return true;
+                        }
+                        if (dataSource.getName().toLowerCase().contains(newValue.toLowerCase())) {
+                            return true;
+                        }
+                        //Do not let already selected stuff disappear.
+                        return false;
+                    });
                 }
-                if(dataSource.getName().toLowerCase().contains(newValue.toLowerCase())){
-                    return true;
-                }
-                //Do not let already selected stuff disappear.
-                return false;
-            });
-        }
         );
 
 
@@ -112,21 +131,22 @@ public class DataSourceSelectionControl implements Initializable{
     }
 
     @FXML
-    private void btnOkayClick(){
+    private void btnOkayClick() {
         ObservableList<DataSource> selectedDataSources = dataSourceSelectionTable.getSelectionModel().getSelectedItems();
-        if(selectedDataSources.size()==0){
+        if (selectedDataSources.size() == 0) {
             Alert alert = new Alert(Alert.AlertType.WARNING, "No data source selected");
             alert.getDialogPane().getStylesheets().add("/gui/darkTheme.css");
             alert.showAndWait();
             return;
         }
-        dataVisualization.display((VisualizationControl.PresentationMode) presentationMode.getSelectionModel().getSelectedItem(),selectedDataSources);
+        dataVisualization.display((VisualizationControl.PresentationMode) presentationMode.getSelectionModel().getSelectedItem(), selectedDataSources);
         Stage stage = (Stage) presentationMode.getScene().getWindow();
         stage.close();
     }
 
     /**
      * Select all dataSources.
+     *
      * @param mouseEvent
      */
     public void selectAll(MouseEvent mouseEvent) {
@@ -135,6 +155,7 @@ public class DataSourceSelectionControl implements Initializable{
 }
 
 //TODO think about moving this interface to its own file or to visualization.VisualizationControl
+
 /**
  * To communicate which data is displayed how.
  * Example: Humidity Sensor 1 as Line Graph
